241da557f882d2aa1d2feda51fd1baa14b432754
EliasVGZ <elias.bs2@hotmail.com>
2023-11-14 14:28:29 +0100

    añadiendo nuevo modelo suceso

--- models/informacion.py
+++ models/informacion.py
@@ -6,13 +6,13 @@ class informacion(models.Model):
     _name = 'odoo_basico.informacion'
     _description = 'Exemplo informacion'
 
-    name = fields.Char(required=True, size=20, string="EScriba algo para odoo_bsaico:")
+    name = fields.Char(required=True, size=20, string="Escriba algo para odoo_basico:")
     descripcion = fields.Text(string="A descripcion: ")
     alto_en_cms = fields.Integer(string="Alto en centimetros: ")
     longo_en_cms = fields.Integer(string="Longo en centimetros: ")
     ancho_en_cms = fields.Integer(string="Ancho en centimetros: ")
     peso = fields.Float(digits=(6,2), default = 2.7, string="Peso en Kgs: " )
     autorizado = fields.Boolean(string="¿Autorizado?", default=True)
-    sexo_traducido = fields.Selection([('Home', 'Hombre'), ('Muller', 'Mujer'), ('Otros', 'otros')], string="Sexo")
+    sexo_traducido = fields.Selection([('Hombre', 'Home'), ('Mujer', 'Muller'), ('Otros', 'otros')], string="Sexo")
 
 
