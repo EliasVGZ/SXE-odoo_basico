3ea44a807e379fa2055a90bad3d6129611b21002
EliasVGZ <elias.bs2@hotmail.com>
2024-01-10 14:17:16 +0100

    moneda

--- models/informacion.py
+++ models/informacion.py
@@ -8,7 +8,7 @@ class informacion(models.Model):
     _sql_constraints = [('nomeUnico', 'unique(name)', 'O Titulo xa existe na Base de Datos')] #RESTRICCION SQL, en este caso por el nombre, no pueden repetir el titulo
     _order = "descripcion desc" #Se ordenada descendente
 
-    name = fields.Char(required=True, size=20, string="Escriba algo para odoo_basico:")
+    name = fields.Char(required=True, size=20, string="Titulo")
     descripcion = fields.Text(string="A descripcion: ")
     alto_en_cms = fields.Integer(string="Alto en centimetros: ")
     longo_en_cms = fields.Integer(string="Longo en centimetros: ")
@@ -19,8 +19,30 @@ class informacion(models.Model):
     densidade = fields.Float(compute="_densidade", store="True", string="Densidade en KG/m3")
     autorizado = fields.Boolean(string="¿Autorizado?", default=True)
     sexo_traducido = fields.Selection([('Hombre', 'Home'), ('Mujer', 'Muller'), ('Otros', 'otros')], string="Sexo")
+    foto = fields.Binary(string='Foto')
+    adxunto_nome = fields.Char(string="Nome Adxunto")
+    adxunto = fields.Binary(string="Arquivo adxunto")
 
+    # Os campos Many2one crean un campo na BD
+    moeda_id = fields.Many2one('res.currency', string="Moneda ELIAS", domain="[('position','=','after')]")
+    # con domain, filtramos os valores mostrados. Pode ser mediante unha constante (vai entre comillas) ou unha variable
     # Cuando se cambien alguno de los campos vaise a ejecutar este metodo
+
+    # PARA VER LA MONEDA EN MODO TEXTO
+    moeda_en_texto = fields.Char(related="moeda_id.currency_unit_label",
+                                 string="Moeda en formato texto", store=True)
+
+    moeda_euro_id = fields.Many2one('res.currency',
+                                    default=lambda self: self.env['res.currency'].search([('name', '=', "EUR")],limit=1))
+    gasto_en_euros = fields.Monetary("Gasto en Euros", 'moeda_euro_id')
+
+
+
+#PARA SABER QUIEN ES EL USUARIO CREADOR
+    creador_da_moeda = fields.Char(related="moeda_id.create_uid.login",
+                                   string="Usuario creador da moeda", store=True)
+
+
     @api.depends('alto_en_cms', 'longo_en_cms', 'ancho_en_cms')
     def _volume(self):
         for rexistro in self:
